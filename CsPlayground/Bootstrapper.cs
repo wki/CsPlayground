997059776f5ef1c05a174e128de308309a2e45ce
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2015-01-09 21:22:13 +0100

    created windsor installer class, simplified windsor setup

---
+++ CsPlayground/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using Castle.Core;
+using Castle.Core.Logging;
+using Castle.DynamicProxy;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using System;
+
+namespace CsPlayground
+{
+    /// <summary>
+    /// Will be automatically called via .Install() by Windsor
+    /// </summary>
+    public class Bootstrapper: IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            Console.WriteLine("Installing Container");
+
+            container.Register(
+                Component.For<IInterceptor>()
+                    .ImplementedBy<LoggingInterceptor>()
+                    .Named("logginginterceptor")
+            );
+            container.Register(
+                Component.For<IAdder>()
+                    .ImplementedBy<Adder>()
+                    .Interceptors(InterceptorReference.ForKey("logginginterceptor")).Anywhere
+            );
+        }
+    }
+}
--- CsPlayground/Start.cs
+++ CsPlayground/Start.cs
@@ -1,13 +1,6 @@
-﻿
-using Castle.Core;
-using Castle.DynamicProxy;
-using Castle.MicroKernel.Registration;
-using Castle.Windsor;
+﻿using Castle.Windsor;
+using Castle.Windsor.Installer;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CsPlayground
 {
@@ -17,19 +10,11 @@ namespace CsPlayground
         {
             Console.WriteLine("Starting Main");
 
-            var container = new WindsorContainer();
-            container.Register(
-                Component.For<IInterceptor>()
-                    .ImplementedBy<LoggingInterceptor>()
-                    .Named("logginginterceptor")
-            );
-            container.Register(
-                Component.For<IAdder>()
-                    .ImplementedBy<Adder>()
-                    .Interceptors(InterceptorReference.ForKey("logginginterceptor")).Anywhere
-            );
+            var container = new WindsorContainer()
+                .Install(FromAssembly.This());
+
             var adder = container.Resolve<IAdder>();
-            
+
             adder.Add1(3);
 
             Console.WriteLine("Ending Main");
